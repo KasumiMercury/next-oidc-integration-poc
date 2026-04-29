426769fa40c49fe2ff3c00c207639535b5f2bf5c
KasumiMercury <naoki_shira@outlook.jp>
2026-04-29 22:29:06 +0900

    chore: config better-auth

--- front/src/lib/auth/auth.ts
+++ front/src/lib/auth/auth.ts
@@ -1,10 +1,33 @@
 import { drizzleAdapter } from "@better-auth/drizzle-adapter";
 import { betterAuth } from "better-auth";
+import { nextCookies } from "better-auth/next-js";
+import { genericOAuth } from "better-auth/plugins";
 import { db } from "@/lib/db";
+import { env } from "@/lib/env";
 
 // pnpm dlx auth@latest generate --config ./src/lib/auth/auth.ts --output ./src/lib/db/schema.ts --yes
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
 		provider: "sqlite",
 	}),
+	baseURL: env.betterAuthUrl,
+	secret: env.betterAuthSecret,
+	plugins: [
+		genericOAuth({
+			config: [
+				{
+					providerId: "first",
+					clientId: env.oidcClientId,
+					clientSecret: env.oidcClientSecret,
+					authorizationUrl: `${env.oidcIssuerUrl}/oauth2/auth`,
+					tokenUrl: `${env.hydraPublicUrl}/oauth2/token`,
+					userInfoUrl: `${env.hydraPublicUrl}/userinfo`,
+					issuer: env.oidcIssuerUrl,
+					scopes: env.oidcScope.split(" "),
+					pkce: true,
+				},
+			],
+		}),
+		nextCookies(),
+	],
 });
